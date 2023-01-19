a234b6d97ec70b432cc9a38ae6a1560dd86f1062
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-01-19 13:46:36 +0300

    Begin

--- BelalBinRabah/src/HomeFirstPage/components/About/About.jsx
+++ BelalBinRabah/src/HomeFirstPage/components/About/About.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import Contact from "./Contact/Contact";
+import { Routes, Router, Route, Link } from "react-router-dom";
+import "./About.css";
+const LazyAbout = React.lazy(() => import("./About/AboutUs"));
 
 export default function About() {
   return (
     <>
-        <h1 id='hi'>Hello</h1>
+      <div>
+        <h1>This is the home page</h1>
+        <Link to='/'>Click to view our page</Link>
+        <Link to="Contact">Wana Make a quick call📞🤙</Link>
+      </div>
+      <div>
+        <Routes>
+          <Route
+            path='/'
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
+// import React from 'react'
+// const LazyAbout = React.lazy(() => import('./About/About'))
+// import {Routes, Router, Route, Link} from 'react-router-dom'
+// import "./Comp1.css"
+
+// export default function Comp1() {
+//   return (
+//     <>
+//     <div>
+//       <h1>This is the home page</h1>
+//       <Link to="/">Click to view our about page</Link>
+//       <Link to="Login">Click to view our contact page</Link>
+//     </div>
+//         <Routes>
+//           <Route
+//             path='/'
+//             element={
+//               <React.Suspense fallback="hi">
+//                 <LazyAbout/>
+//               </React.Suspense>
+//             }/>
+//           <Route path='Login' element={<Login/>}/>
+//         </Routes>
+//     </>
+//   )
+// }
---
+++ BelalBinRabah/src/HomeFirstPage/components/About/About/AboutUs.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './AboutUs.css'
+export default function AboutUs() {
+  return (
+    <h1>This is working -from Aboutus</h1>
+  )
+}
+
