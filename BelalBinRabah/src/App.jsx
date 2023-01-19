8602c5243872bdc078d0dee95a4e58f5ae72b699
Eslamzaid <113968253+Eslamzaid@users.noreply.github.com>
2023-01-19 13:26:52 +0300

    Impr

--- BelalBinRabah/src/App.jsx
+++ BelalBinRabah/src/App.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import HomeFirst from "./HomeFirstPage/HomeFirst"
-import Loading from './Loading/Loading'
-import "./App.css"
+import React from "react";
+import HomeFirst from "./HomeFirstPage/HomeFirst";
+import Loading from "./Loading/Loading";
+import "./App.css";
 
 export default function App() {
-  const [isLoading, setIsLoading] = React.useState(true)
+  const [isLoading, setIsLoading] = React.useState(true);
   React.useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false)
-    }, 2000)
-  })
+      setIsLoading(false);
+    }, 2000);
+  });
 
-  return (
-    <div>
-      {
-        isLoading ? <Loading/> : <HomeFirst/>
-      }
-    </div>
-  )
+  return <div>{isLoading ? <Loading /> : <HomeFirst />}</div>;
 }
-
